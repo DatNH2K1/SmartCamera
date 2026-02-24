ca618484a6a153c96c08ca322b2634a4ed44dd25
datnghiem <datnh@trustring.vn>
2026-02-24 21:33:10 +0700

    Fix style

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: './', // Ensures relative paths for assets so it works on any subdirectory (GitHub Pages)
   build: {
     outDir: 'dist',
